17329231bf6d59a65a2bc6723740eed6cb748d67
anoopraju31 <anoopraju31@gmail.com>
2024-03-31 12:46:26 +0530

    Selecteble Grid: SelectableGrid component - initial

--- selectable-grid/src/App.jsx
+++ selectable-grid/src/App.jsx
@@ -1,7 +1,10 @@
+import SelectableGrid from './components/SelectableGrid'
+
 export default function App() {
 	return (
 		<div>
-			<h1 className='text-3xl font-bold underline'>Hello world!</h1>
+			<h1 className='text-3xl font-bold text-center my-5'> Selectable Grid </h1>
+			<SelectableGrid />
 		</div>
 	)
 }
---
+++ selectable-grid/src/components/SelectableGrid.jsx
@@ -0,0 +1,3 @@
+export default function SelectableGrid() {
+	return <div>SelectableGrid</div>
+}
