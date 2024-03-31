fb64559c32a167b6297663ba7854d70f74db104d
anoopraju31 <anoopraju31@gmail.com>
2024-03-31 13:33:41 +0530

    Selecteble Grid: SelectableGrid component - logic added

--- selectable-grid/src/components/SelectableGrid.jsx
+++ selectable-grid/src/components/SelectableGrid.jsx
@@ -1,17 +1,58 @@
 import PropTypes from 'prop-types'
+import { useCallback, useState } from 'react'
 
 export default function SelectableGrid({ rows = 10, columns = 10 }) {
+	const [isMouseDown, setIsMouseDown] = useState(false)
+	const [selectedBox, setSelectedBox] = useState([])
+
+	const handleMouseUp = () => setIsMouseDown(false)
+	const handleMouseDown = (boxId) => {
+		setIsMouseDown(true)
+		setSelectedBox([boxId])
+	}
+	const handleMouseEnter = useCallback(
+		(boxId) => {
+			if (!isMouseDown) return
+
+			const startBox = selectedBox[0]
+			const endBox = boxId
+
+			const startRow = Math.floor((startBox - 1) / columns)
+			const startColumn = (startBox - 1) % columns
+			const endRow = Math.floor((endBox - 1) / columns)
+			const endColumn = (endBox - 1) % columns
+
+			const minRow = Math.min(startRow, endRow)
+			const maxRow = Math.max(startRow, endRow)
+			const minColumn = Math.min(startColumn, endColumn)
+			const maxColumn = Math.max(startColumn, endColumn)
+
+			const selected = []
+
+			for (let row = minRow; row < maxRow; row++)
+				for (let column = minColumn; column < maxColumn; column++)
+					selected.push(row * columns + column + 1)
+
+			setSelectedBox(selected)
+		},
+		[isMouseDown],
+	)
 	return (
 		<div
 			style={{
 				gridTemplateColumns: `repeat(${columns}, 40px)`,
 				gridTemplateRows: `repeat(${rows}, 40px)`,
 			}}
-			className={`grid `}>
+			className='grid'
+			onMouseUp={handleMouseUp}>
 			{[...Array(rows * columns).keys()].map((_, i) => (
 				<div
 					key={i}
-					className='border border-black flex justify-center items-center'>
+					onMouseDown={() => handleMouseDown(i + 1)}
+					onMouseEnter={() => handleMouseEnter(i + 1)}
+					className={`border border-black select-none flex justify-center items-center ${
+						selectedBox.includes(i + 1) ? 'bg-green-400' : 'bg-white'
+					}`}>
 					{i + 1}
 				</div>
 			))}
