2bb60da2ab9ee69571b473c1a429a152bbb4fea4
anoopraju31 <anoopraju31@gmail.com>
2024-03-31 13:03:48 +0530

    Selecteble Grid: SelectableGrid component - styled

--- selectable-grid/src/App.jsx
+++ selectable-grid/src/App.jsx
@@ -2,9 +2,9 @@ import SelectableGrid from './components/SelectableGrid'
 
 export default function App() {
 	return (
-		<div>
+		<div className='flex flex-col items-center'>
 			<h1 className='text-3xl font-bold text-center my-5'> Selectable Grid </h1>
-			<SelectableGrid />
+			<SelectableGrid rows={15} columns={15} />
 		</div>
 	)
 }
--- selectable-grid/src/components/SelectableGrid.jsx
+++ selectable-grid/src/components/SelectableGrid.jsx
@@ -1,3 +1,18 @@
-export default function SelectableGrid() {
-	return <div>SelectableGrid</div>
+export default function SelectableGrid({ rows = 10, columns = 10 }) {
+	return (
+		<div
+			style={{
+				gridTemplateColumns: `repeat(${columns}, 40px)`,
+				gridTemplateRows: `repeat(${rows}, 40px)`,
+			}}
+			className={`grid `}>
+			{[...Array(rows * columns).keys()].map((_, i) => (
+				<div
+					key={i}
+					className='border border-black flex justify-center items-center'>
+					{i + 1}
+				</div>
+			))}
+		</div>
+	)
 }
